da243dc4f4a059c611353dea1f6395195b8a8d14
Hannah Mowbray <hannahmowbray3@gmail.com>
2025-04-05 10:39:05 +0100

    set up rough page outline
    Signed-off-by: Hannah Mowbray <hannahmowbray3@gmail.com>

---
+++ src/app/add-youtube/page.tsx
@@ -0,0 +1,7 @@
+export default function AddYoutube() {
+  return (
+    <h1>
+      Here is where you will upload a youtube link of the weekly recording
+    </h1>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import NavBar from "../(components)/nav-bar";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        <Link href="/add-blog">Add a blog post</Link>
+      </div>
+      <div>
+        <Link href="/upload-images">Upload images</Link>
+      </div>
+      <div>
+        <Link href="/add-youtube">Add a new youtube link</Link>
+      </div>
+    </>
+  );
 }
